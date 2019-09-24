0757d33fcb34fb374442cab638e811131e5cfbfd
coffeecoke <18601376034@163.com>
2019-09-24 17:46:17 +0800

    结构雏形

--- js/chartFactory.js
+++ js/chartFactory.js
@@ -1,49 +1,92 @@
 (function (root, factory) {
   root.a = root.a || factory(Jquery)
+
+
 })(this, function ($) {
   function ChartFactory(opts) {
     this.init(opts)
-    this.initData()
   }
   ChartFactory._defaultOpts = {
     asy: false,
     data: [],
-    url:'',
+    url: '',
+    themeType:'macarons'
 
   }
   ChartFactory.prototype.init = function (opts) {
-    this.opts= $.extend(ChartFactory._defaultOpts,opts);
+
+    this.opts = $.extend(ChartFactory._defaultOpts, opts);
+    this.setChartTheme (this.opts.themeType)
+    this.setChartOptionTemplates();
   }
   ChartFactory.prototype.initData = function () {
     var _self = this;
     if (this.opts.asy) {
       $.ajax({
-        url:this.opts.url,
-        type:'post',
-        success:function (result) {
+        url: this.opts.url,
+        type: 'post',
+        success: function (result) {
           _self.opts.data = result;
-          _self.chartDataFormate(_self.opts.data)
+
         }
       })
-      
+
     } else {
-      _self.chartDataFormate(_self.opts.data)
-      
+
+
     }
 
   }
-  ChartFactory.prototype.setChartConfig = function () {
 
+
+  ChartFactory.prototype.setChartTheme = function (themeType) {
+    var themes = {
+      macarons:''// 配置主题的路径
+    }
+    this.chart = echarts.init(document.getElementById('main'),themes[themeType]);
   }
   ChartFactory.prototype.chartDataFormate = function (data) {
+    
 
   }
   ChartFactory.prototype.setChartOptionTemplates = function () {
+    var _self = this;
+    var chartOptionTemplates = {
+      CommonOption: { //通用的图表基本配置
+        tooltip: {
+          trigger: 'axis' //tooltip触发方式:axis以X轴线触发,item以每一个数据项触发
+        },
+        toolbox: {
+          show: false, //是否显示工具栏
+          feature: {
+            mark: true,
+            dataView: {
+              readOnly: false
+            }, //数据预览
+            restore: true, //复原
+            saveAsImage: true //是否保存图片
+          }
+        }
+      },
+      pie: function (title) {
+        _self.chartDataFormate(_self.opts.data, title)
+        _self.renderChart()
+      },
+      lines: function (title, stack) {
+        _selft.chartDataFormate(_self.opts.data, title, stack)
+        _self.renderChart()
+      },
+      bars: function (title, stack) {
+        _selft.chartDataFormate(_self.opts.data, title, stack)
+        _self.renderChart()
+      },
+      map:function () {
+        _self.renderMap()
+      }
 
+    }
+    return chartOptionTemplates
   }
-
-  
-
   ChartFactory.prototype.renderChart = function () {
 
   }
@@ -52,6 +95,10 @@
   }
 
   ChartFactory.prototype.resize = function () {
+    var _self = this;
+    $(window).on('resize',function () {
+      _self.resize()
+    })
 
   }
 
