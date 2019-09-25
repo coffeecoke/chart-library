79f84dd376ca0e18acc853f56ebb2fddc759e894
coffeecoke <18601376034@163.com>
2019-09-25 15:57:55 +0800

    完善代码结构

--- js/chartFactory.js
+++ js/chartFactory.js
@@ -1,117 +1,149 @@
 (function (root, factory) {
-  
+
   if (typeof define === 'function' && define.amd) {
-      // AMD
-      // define([], factory);
-      console.log(222)
-      define(['jquery'],factory);
+    define(['jquery', 'echarts'], factory);
   } else if (typeof exports === 'object') {
-      // Node.js
-      module.exports = factory;
+    // Node.js
+    module.exports = factory;
   } else {
-      // Browser globals
-      root.JSLite = root.JSLite || factory(jQuery);
+    // Browser globals
+    root.JSLite = root.JSLite || factory(jQuery);
   }
-})(this, function ($) {
+})(this, function ($, echarts) {
   function ChartFactory(opts) {
     this.init(opts)
+
   }
   ChartFactory._defaultOpts = {
+    id: '',
     asy: false,
     data: [],
     url: '',
-    themeType:'macarons'
+    themeType: ''
 
   }
-  ChartFactory.prototype.init = function (opts) {
-
-    this.opts = $.extend(ChartFactory._defaultOpts, opts);
-    this.setChartTheme (this.opts.themeType)
-    this.setChartOptionTemplates();
-  }
-  ChartFactory.prototype.initData = function () {
-    var _self = this;
-    if (this.opts.asy) {
-      $.ajax({
-        url: this.opts.url,
-        type: 'post',
-        success: function (result) {
-          _self.opts.data = result;
-
-        }
-      })
-
-    } else {
-
-
-    }
-
-  }
-
-
-  ChartFactory.prototype.setChartTheme = function (themeType) {
-    var themes = {
-      macarons:''// 配置主题的路径
-    }
-    this.chart = echarts.init(document.getElementById('main'),themes[themeType]);
-  }
-  ChartFactory.prototype.chartDataFormate = function (data) {
-
-
-  }
-  ChartFactory.prototype.setChartOptionTemplates = function () {
-    var _self = this;
-    var chartOptionTemplates = {
-      CommonOption: { //通用的图表基本配置
-        tooltip: {
-          trigger: 'axis' //tooltip触发方式:axis以X轴线触发,item以每一个数据项触发
-        },
-        toolbox: {
-          show: false, //是否显示工具栏
-          feature: {
-            mark: true,
-            dataView: {
-              readOnly: false
-            }, //数据预览
-            restore: true, //复原
-            saveAsImage: true //是否保存图片
-          }
-        }
-      },
-      pie: function (title) {
-        _self.chartDataFormate(_self.opts.data, title)
-        _self.renderChart()
-      },
-      lines: function (title, stack) {
-        _selft.chartDataFormate(_self.opts.data, title, stack)
-        _self.renderChart()
-      },
-      bars: function (title, stack) {
-        _selft.chartDataFormate(_self.opts.data, title, stack)
-        _self.renderChart()
-      },
-      map:function () {
-        _self.renderMap()
+  var chartCommonOption = { //通用的图表基本配置
+    tooltip: {
+      trigger: 'axis' //tooltip触发方式:axis以X轴线触发,item以每一个数据项触发
+    },
+    toolbox: {
+      show: false, //是否显示工具栏
+      feature: {
+        mark: true,
+        dataView: {
+          readOnly: false
+        }, //数据预览
+        restore: true, //复原
+        saveAsImage: true //是否保存图片
       }
-
     }
-    return chartOptionTemplates
-  }
-  ChartFactory.prototype.renderChart = function () {
-
-  }
-  ChartFactory.prototype.renderMap = function () {
-
   }
 
-  ChartFactory.prototype.resize = function () {
-    var _self = this;
-    $(window).on('resize',function () {
-      _self.resize()
-    })
+  var chartDataFormate = {
+    FormateNOGroupData: function (data) { //data的格式如上的Result1，这种格式的数据，多用于饼图、单一的柱形图的数据源
+      var categories = [];
+      var datas = [];
+      for (var i = 0; i < data.length; i++) {
+        categories.push(data[i].name || "");
+        datas.push({
+          name: data[i].name,
+          value: data[i].value || 0
+        });
+      }
+      return {
+        category: categories,
+        data: datas
+      };
+    },
 
   }
 
+  var chartOptionTemplates = {
+    pie: function (name) {
+      var pie_datas = chartDataFormate.FormateNOGroupData(this.opts.data);
+      var option = {
+        tooltip: {
+          trigger: 'item',
+          formatter: '{b} : {c} ({d}/%)',
+          show: true
+        },
+        legend: {
+          orient: 'vertical',
+          x: 'left',
+          data: pie_datas.category
+        },
+        series: [{
+          name: name || "",
+          type: 'pie',
+          radius: '65%',
+          center: ['50%', '50%'],
+          data: pie_datas.data
+        }]
+      };
+      var pieOptions =  $.extend({}, chartCommonOption, option);
+      this.renderChart(pieOptions)
+    },
+    lines: function (name, stack) {
+      this.chartDataFormate(_self.opts.data, title, stack)
+      this.renderChart()
+    },
+    bars: function (name, stack) {
+      this.chartDataFormate(_self.opts.data, title, stack)
+      this.renderChart()
+    },
+    map: function () {
+      this.renderMap()
+    }
 
+  }
+  ChartFactory.prototype = {
+    init: function (opts) {
+      this.opts = $.extend(ChartFactory._defaultOpts, opts);
+      this.setChartTheme(this.opts.themeType)
+      this.setChartOptionTemplates();
+    },
+    initData: function () {
+      var _self = this;
+      if (this.opts.asy) {
+        $.ajax({
+          url: this.opts.url,
+          type: 'post',
+          success: function (result) {
+            _self.opts.data = result;
+          }
+        })
+      } else {
+
+      }
+    },
+    setChartTheme: function (themeType) {
+      var themes = {
+        macarons: '' // 配置主题的路径
+      }
+      if (!this.opts.id) {
+        return
+      }
+      this.chart = echarts.init(document.getElementById(this.opts.id), themes[themeType]);
+    },
+    chartDataFormate: function (data) {
+
+    },
+    setChartOptionTemplates: function () {
+      $.extend(ChartFactory.prototype, chartOptionTemplates)
+    },
+    renderChart: function (chartOptions) {
+      this.chart.setOption(chartOptions)
+      this.resize()
+    },
+    renderMap: function () {
+
+    },
+    resize: function () {
+      var _self = this;
+      $(window).on('resize', function () {
+        _self.chart.resize()
+      })
+    }
+  }
   return ChartFactory
 })
--- js/require.config.js
+++ js/require.config.js
@@ -3,6 +3,7 @@ require.config({
   paths:{
       //RequireJS默认假定所有的依赖资源都是JS脚本，因此无需再module ID上再加上js后缀。
       'jquery':"jquery-2.1.4.min",
-      'chartFactory':'chartFactory'
+      'chartFactory':'chartFactory',
+      'echarts':'echarts.min'
   }
 });
